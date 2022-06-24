3c2acb80e18e2dcfc1a961ebe8fc1914d213069a
jake.k <jake.k@hpcnt.com>
2022-06-24 15:18:17 +0900

    Initial Commit

---
+++ ExInstagramProfile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExInstagramProfile
+//
+//  Created by Jake.K on 2022/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
